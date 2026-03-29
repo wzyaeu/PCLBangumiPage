e7098e5048fbd8bab57d0d31b5e36d0a98e679b9
wzyaeu <wzyaeu@163.com>
2026-03-29 21:24:18 +0800

    anime_rank分页

--- build/anime_rank.py
+++ build/anime_rank.py
@@ -4,22 +4,51 @@ import json
 import requests
 
 def anime_rank_build():
-    savef('anime_rank.json',json.dumps(
-        {
-            "Title": "PCL Bangumi 动漫排行榜"
-        }
-    ,ensure_ascii=False))
 
     print(f'anime_rank - 获取页面内容')
-    data = requests.get('https://api.bgm.tv/v0/subjects?type=2&sort=rank', headers=HEADER)
-    if data.status_code != 200:
-        print(f'anime_rank - 无法获取api数据 ({data.status_code})')
-        exit()
-    json_data = json.loads(data.text)
+    
+    limit = 25
+    pages = 4
 
     t = gett('anime_rank')
-    savef('anime_rank.xaml',
-        y(t\
-        .replace('{{style}}',gett('anime_rank/_style'))\
-        .replace('{{rank}}',anime_rank_rank_get(json_data)))
-    )
+    for p in range(pages):
+        print(f'anime_rank - 获取页面 {p+1}')
+        data = requests.get(f'https://api.bgm.tv/v0/subjects?type=2&sort=rank&limit={limit}&offset={p+1}', headers=HEADER)
+        if data.status_code != 200:
+            print(f'anime_rank - 无法获取页面api数据 ({data.status_code})')
+            exit()
+        json_data = json.loads(data.text)
+
+        pagebtn = gett('anime_rank/pagebtn')
+        pagebtn = pagebtn\
+        .replace('{{p}}',str(p+1))\
+        .replace('{{a}}',str(pages))
+        if p != 0:
+            pagebtn = pagebtn\
+            .replace('{{lbtn}}',\
+                     gett('anime_rank/pagebtn/left').replace('{{num}}',str(p))
+            )
+        else:
+            pagebtn = pagebtn\
+            .replace('{{lbtn}}','')
+
+        if p+1 != pages:
+            pagebtn = pagebtn\
+            .replace('{{rbtn}}',\
+                     gett('anime_rank/pagebtn/right').replace('{{num}}',str(p+2))
+            )
+        else:
+            pagebtn = pagebtn\
+            .replace('{{rbtn}}','')
+
+        savef(f'anime_rank_{p+1}.json',json.dumps(
+            {
+                "Title": f"PCL Bangumi 动漫排行榜 | 第 {p+1} / {pages} 页"
+            }
+        ,ensure_ascii=False))
+        savef(f'anime_rank_{p+1}.xaml',
+            y(t\
+            .replace('{{style}}',gett('anime_rank/_style'))\
+            .replace('{{rank}}',anime_rank_rank_get(json_data, p*limit))\
+            .replace('{{pagebtn}}',pagebtn))
+        )
--- build/anime_rank_rank.py
+++ build/anime_rank_rank.py
@@ -1,13 +1,13 @@
-from tool import gett, savef
+from tool import gett
 
 from .anime_rank_rank_item import anime_rank_rank_item_get
 
-def anime_rank_rank_get(json_data):
+def anime_rank_rank_get(json_data, offset):
     print(f'anime_rank_rank - 获取排行榜内容')
     t = gett('anime_rank/rank')
 
     o = ''
-    for index, item in enumerate(json_data['data'], start=1):
+    for index, item in enumerate(json_data['data'], start=offset+1):
         o += anime_rank_rank_item_get(item, index)
 
     return t\
